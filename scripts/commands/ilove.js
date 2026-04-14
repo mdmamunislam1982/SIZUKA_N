7671e752c6bcd8e31ed25163f9d21a410525d485
Md Mamun Islam <mamunofficial3210@gmail.com>
2026-04-14 21:10:45 +0600

    Update love message in ilove.js

--- scripts/commands/ilove.js
+++ scripts/commands/ilove.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   if (event.body.indexOf("I love you")==0 || (event.body.indexOf("i love you")==0 || (event.body.indexOf("I love u")==0 || (event.body.indexOf("I love you")==0)))) {
     var msg = {
-        body: "Hmm... বস ফারহান ও তোমাকে ভালোবাসে😇😻 :))"
+        body: "Hmm... বস মামুন ও তোমাকে ভালোবাসে😇😻 :))"
     }
       api.sendMessage(msg, threadID, messageID);
     }
