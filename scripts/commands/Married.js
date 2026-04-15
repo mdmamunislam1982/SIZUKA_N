0e5fa0cc224f28ca7afdb8b8be877107473432c1
Md Mamun Islam <mamunofficial3210@gmail.com>
2026-04-15 19:41:35 +0600

    Update Married.js

--- scripts/commands/Married.js
+++ scripts/commands/Married.js
@@ -1,76 +1,104 @@
+const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
+const jimp = require("jimp");
+
 module.exports.config = {
   name: "married",
-  version: "2.0.0",
+  version: "3.0.0",
   permission: 0,
   prefix: true,
-  credits: "Clarence DK",
-  description: "Sends a hug image",
+  credits: "Clarence DK + Edited by Mamun",
+  description: "Generate married image with mention",
   category: "img",
-  usage: "[@mention]",
-  cooldown: 5,
-  dependencies: {
-    "axios": "latest",
-    "fs-extra": "latest",
-    "path": "latest",
-    "jimp": "latest"
-  }
+  usage: "@mention",
+  cooldown: 5
 };
 
 module.exports.onLoad = async () => {
-  const { resolve } = global.nodemodule["path"];
-  const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-  const { downloadFile } = global.utils;
-  const dirMaterial = __dirname + `/cache/canvas/`;
-  const path = resolve(__dirname, 'cache', 'canvas', 'married.png');
-  if (!existsSync(dirMaterial)) mkdirSync(dirMaterial, { recursive: true });
-  if (!existsSync(path)) await downloadFile("https://i.ibb.co/PjWvsBr/13bb9bb05e53ee24893940892b411ad2.png", path);
+  const dirPath = path.join(__dirname, "cache", "canvas");
+  const imgPath = path.join(dirPath, "married.png");
+
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true });
+  }
+
+  if (!fs.existsSync(imgPath)) {
+    const res = await axios.get(
+      "https://i.ibb.co/PjWvsBr/13bb9bb05e53ee24893940892b411ad2.png",
+      { responseType: "arraybuffer" }
+    );
+    fs.writeFileSync(imgPath, Buffer.from(res.data));
+  }
 };
 
-async function makeImage({ one, two }) {
-  const fs = global.nodemodule["fs-extra"];
-  const path = global.nodemodule["path"];
-  const axios = global.nodemodule["axios"]; 
-  const jimp = global.nodemodule["jimp"];
-  const __root = path.resolve(__dirname, "cache", "canvas");
-
-  let batgiam_img = await jimp.read(__root + "/married.png");
-  let pathImg = __root + `/batman${one}_${two}.png`;
-  let avatarOne = __root + `/avt_${one}.png`;
-  let avatarTwo = __root + `/avt_${two}.png`;
-
-  let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
-  fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'binary'));
-
-  let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
-  fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'binary'));
-
-  let circleOne = await jimp.read(await circle(avatarOne));
-  let circleTwo = await jimp.read(await circle(avatarTwo));
-  batgiam_img.composite(circleOne.resize(150, 150), 280, 45).composite(circleTwo.resize(150, 150), 130, 90);
-
-  let raw = await batgiam_img.getBufferAsync(jimp.MIME_PNG);
-
-  fs.writeFileSync(pathImg, raw);
-  fs.unlinkSync(avatarOne);
-  fs.unlinkSync(avatarTwo);
-
-  return pathImg;
+async function circle(imgPath) {
+  const img = await jimp.read(imgPath);
+  img.circle();
+  return await img.getBufferAsync(jimp.MIME_PNG);
 }
 
-async function circle(image) {
-  const jimp = require("jimp");
-  image = await jimp.read(image);
-  image.circle();
-  return await image.getBufferAsync(jimp.MIME_PNG);
+async function makeImage(uid1, uid2) {
+  const basePath = path.join(__dirname, "cache", "canvas");
+
+  const bg = await jimp.read(path.join(basePath, "married.png"));
+
+  const avt1Path = path.join(basePath, `avt_${uid1}.png`);
+  const avt2Path = path.join(basePath, `avt_${uid2}.png`);
+  const outPath = path.join(basePath, `married_${uid1}_${uid2}.png`);
+
+  const avt1 = await axios.get(
+    `https://graph.facebook.com/${uid1}/picture?width=512&height=512`,
+    { responseType: "arraybuffer" }
+  );
+  const avt2 = await axios.get(
+    `https://graph.facebook.com/${uid2}/picture?width=512&height=512`,
+    { responseType: "arraybuffer" }
+  );
+
+  fs.writeFileSync(avt1Path, Buffer.from(avt1.data));
+  fs.writeFileSync(avt2Path, Buffer.from(avt2.data));
+
+  const circle1 = await jimp.read(await circle(avt1Path));
+  const circle2 = await jimp.read(await circle(avt2Path));
+
+  bg.composite(circle1.resize(150, 150), 280, 45);
+  bg.composite(circle2.resize(150, 150), 130, 90);
+
+  const buffer = await bg.getBufferAsync(jimp.MIME_PNG);
+  fs.writeFileSync(outPath, buffer);
+
+  fs.unlinkSync(avt1Path);
+  fs.unlinkSync(avt2Path);
+
+  return outPath;
 }
 
-module.exports.run = async function ({ event, api, args }) {    
-  const fs = global.nodemodule["fs-extra"];
-  const { threadID, messageID, senderID } = event;
-  const mention = Object.keys(event.mentions);
-  if (!mention[0]) return api.sendMessage("Please mention 1 person.", threadID, messageID);
-  else {
-    const one = senderID, two = mention[0];
-    return makeImage({ one, two }).then(path => api.sendMessage({ body: "যা্ঁ তু্ঁমা্ঁর্ঁ তো্ঁ বি্ঁয়ে্ঁ ক্ঁরে্ঁ ফে্ঁল্ঁলো্ঁ।🤧😆 এ্ঁখ্ঁন্ঁ বা্ঁস্ঁর্ঁ রা্ঁত্ঁ ক্ঁরা্ঁর্ঁ জ্ঁন্য্ঁ রে্ঁডি্ঁ হ্ঁও্ঁ।🤭🤣", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+module.exports.run = async function ({ event, api }) {
+  const { threadID, messageID, senderID, mentions } = event;
+
+  const mentionID = Object.keys(mentions);
+  if (!mentionID[0]) {
+    return api.sendMessage("❌ একজনকে mention করো", threadID, messageID);
   }
-    }
+
+  const one = senderID;
+  const two = mentionID[0];
+
+  try {
+    const imgPath = await makeImage(one, two);
+
+    api.sendMessage(
+      {
+        body: "😆 তোমার তো বিয়ে হয়ে গেল! 🤧💍 এখন বাসর রাতের প্রস্তুতি নাও 🤭🤣",
+        attachment: fs.createReadStream(imgPath)
+      },
+      threadID,
+      () => fs.unlinkSync(imgPath),
+      messageID
+    );
+  } catch (err) {
+    console.log(err);
+    api.sendMessage("❌ ইমেজ বানাতে সমস্যা হয়েছে", threadID, messageID);
+  }
+};
